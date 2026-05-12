58775d71ab7213f09f8cee9b07896b7f4bcb087e
yoanna-b <yoanna.s.barajas@gmail.com>
2026-05-12 10:26:40 -0500

    first iteration added

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -2,10 +2,98 @@ public class RockPaperScissors {
     // Boolean variable which is true until player wants to quit
     public boolean keepPlaying = true;
 
-    /**
-     * Runs the main game loop - calling other functions as needed.
-     * You will create this as for now it just prints out - TBD.
-     */
+        int cMove;
+        int userMove = 0;
+        int cScore = 0;
+        int pScore = 0;
+        int tie = 0;
+        int rounds = 0;
+        Random r = new Random();
+        Scanner input = new Scanner(System.in);
+        cMove = r.nextInt(3) + 1;
+
+
+        while (answer.equalsIgnoreCase("yes")) {
+            System.out.println("Choose your move!");
+            System.out.println("Enter 1 for rock, 2 for paper, or 3 for scissors:");
+            userMove = input.nextInt();
+            while (input.hasNextInt()) {
+                if (userMove != 1 && userMove != 2 && userMove != 3) {
+                    System.out.println("Invalid move. Try again.");
+                    System.out.println("Enter your move: ");
+                    input.nextInt();
+                }
+            }
+            if (userMove == 1) {
+                System.out.println("You have chosen rock!");
+            } else if (userMove == 2) {
+                System.out.println("You have chosen paper!");
+            } else if (userMove == 3) {
+                System.out.println("You have chosen scissors!");
+            }
+
+
+            if (userMove == cMove) {
+                System.out.println("Tie game!");
+                tie++;
+                rounds++;
+            } else if (cMove == 1 && userMove == 3) {
+                System.out.println("Computer chose rock.");
+                System.out.println("Rock beats scissors.");
+                System.out.println("Computer Wins!");
+                cScore++;
+                rounds++;
+            } else if (cMove == 1 && userMove == 2) {
+                System.out.println("Computer chose rock.");
+                System.out.println("Paper beats rock.");
+                System.out.println("You win!");
+                pScore++;
+                rounds++;
+            } else if (cMove == 2 && userMove == 3) {
+                System.out.println("Computer chose rock");
+                System.out.println("Scissors beats paper.");
+                System.out.println("You win!");
+                pScore++;
+                rounds++;
+            } else if (cMove == 2 && userMove == 1) {
+                System.out.println("Computer chose paper.");
+                System.out.println("Paper beats rock.");
+                System.out.println("Computer wins!");
+                cScore++;
+                rounds++;
+            } else if (cMove == 3 && userMove == 1) {
+                System.out.println("Computer chose scissors.");
+                System.out.println("Rock beats scissors.");
+                System.out.println("You win!");
+                pScore++;
+                rounds++;
+            } else if (cMove == 3 && userMove == 2) {
+                System.out.println("Computer chose scissors.");
+                System.out.println("Scissors beats paper.");
+                System.out.println("Computer wins!");
+                cScore++;
+                rounds++;
+            }
+
+
+
+
+            System.out.println("Would you like to play again?");
+            System.out.println("\"Yes\" or \"no\"?");
+            input.next();
+            answer = input.next();
+
+
+            if (answer.equalsIgnoreCase("yes")) {
+                main(null);
+            } else {
+                System.out.println("Here is the final score after " + rounds + "rounds:");
+                System.out.println("You: " + pScore + "Computer: " + cScore + "Ties: " + tie);
+            }
+        }
+    }
+}
+
     public void run() {
         System.out.println("Rock Paper Scissors - TBD");
     }
